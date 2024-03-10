48d62bd6af7d73e4d350cedae29378697b405e62
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-10 22:37:21 +0100

    feat: add NF1Left normalization rule implementation

---
+++ src/main/java/normalisation/rules/NF1Left.java
@@ -0,0 +1,33 @@
+package normalisation.rules;
+
+import org.semanticweb.owlapi.model.*;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
+
+import java.util.Collection;
+import java.util.List;
+
+public class NF1Left implements NormalisationRule {
+    @Override
+    public Collection<OWLSubClassOfAxiom> normalise(IRI ontologyIri, OWLSubClassOfAxiom subClassOfAxiom) {
+        OWLClassExpression subClass = subClassOfAxiom.getSubClass();
+        OWLClassExpression superClass = subClassOfAxiom.getSuperClass();
+
+        if (!(subClass instanceof OWLObjectIntersectionOf objectIntersectionOf) || !superClass.isNamed()) {
+            throw new IllegalArgumentException();
+        }
+
+        List<OWLClassExpression> objectIntersectionOfOperands = objectIntersectionOf.getOperandsAsList();
+        OWLClassExpression leftObjectIntersectionOfOperand = objectIntersectionOfOperands.getFirst();
+        OWLClassExpression rightObjectIntersectionOfOperand = objectIntersectionOfOperands.getLast();
+
+        OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+        OWLClass newClass = owlDataFactory.getOWLClass(ontologyIri + "#" + subClass.hashCode() + superClass.hashCode());
+
+        OWLSubClassOfAxiom firstNewSubClassOfAxiom = owlDataFactory.getOWLSubClassOfAxiom(leftObjectIntersectionOfOperand, newClass);
+
+        OWLObjectIntersectionOf newObjectIntersectionOf = owlDataFactory.getOWLObjectIntersectionOf(newClass, rightObjectIntersectionOfOperand);
+        OWLSubClassOfAxiom secondNewSubClassOfAxiom = owlDataFactory.getOWLSubClassOfAxiom(newObjectIntersectionOf, superClass);
+
+        return List.of(firstNewSubClassOfAxiom, secondNewSubClassOfAxiom);
+    }
+}
