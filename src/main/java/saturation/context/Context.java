e62cc83ca197e4b668043a9b9c0be79b5acb3d94
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-10 23:02:08 +0100

    feat: add Context interface
    To perform ontology saturation concurrently, it is necessary to group axioms into contexts that can be processed by workers independently. A context can only be processed by one thread at a time. If multiple axioms together can generate an inference, then these axioms must necessarily be contained in a common context. A context is active when its queue of to-do axioms is not empty. The operations that expose this interface must be thread-safe. Only 6 contexts can exist in this project. So this is a sealed interface.

---
+++ src/main/java/saturation/context/Context.java
@@ -0,0 +1,24 @@
+package saturation.context;
+
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@ThreadSafe
+public sealed interface Context permits ContextCR1, ContextCR2, ContextCR3, ContextCR4, ContextCR5, ContextCR6 {
+
+    boolean addTodoAxiom(OWLSubClassOfAxiom axiom);
+    Optional<OWLSubClassOfAxiom> pollTodoAxiom();
+    boolean isQueueTodoAxiomsEmpty();
+
+    boolean addProcessedAxiom(OWLSubClassOfAxiom axiom);
+    boolean containsProcessedAxiom(OWLSubClassOfAxiom axiom);
+    Collection<OWLSubClassOfAxiom> getProcessedAxioms();
+
+    void accept(ContextVisitor contextVisitor);
+
+    AtomicBoolean getIsActive();
+}
