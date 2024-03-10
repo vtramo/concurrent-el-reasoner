f343878c9977cef417dbc5328ef04c17b61be68f
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-10 22:47:56 +0100

    test: add OntologyUtils class primarily aimed at testing purposes
    this class is added to provide utility methods for creating OWL ontologies for testing purposes. It includes methods for creating empty ontologies, creating a simple university ontology, and creating a specific OWL ontology

---
+++ src/test/java/utils/OntologyUtils.java
@@ -0,0 +1,110 @@
+package utils;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+
+public class OntologyUtils {
+
+    public static OWLOntology createEmptyOWLOntology() {
+        IRI iri = IRI.create("test");
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+
+        try {
+            return manager.createOntology(iri);
+        } catch (OWLOntologyCreationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static OWLOntology createSimpleOntologyUniversity() {
+        OWLOntology universityOntology = OntologyUtils.createEmptyOWLOntology();
+        OWLOntologyManager owlOntologyManager = universityOntology.getOWLOntologyManager();
+        OWLDataFactory df = owlOntologyManager.getOWLDataFactory();
+        IRI iri = owlOntologyManager.getOntologyDocumentIRI(universityOntology);
+
+        OWLClass course = df.getOWLClass(iri + "#Course");
+        OWLClass person = df.getOWLClass(iri + "#Person");
+        OWLClass UGC = df.getOWLClass(iri + "#UGC");
+        OWLClass PGC = df.getOWLClass(iri + "#PGC");
+        OWLClass teacher = df.getOWLClass(iri + "#Teacher");
+        OWLClass student = df.getOWLClass(iri + "#Student");
+        OWLObjectProperty teaches = df.getOWLObjectProperty(iri + "#teaches");
+        OWLObjectProperty attends = df.getOWLObjectProperty(iri + "#attends");
+
+        OWLObjectIntersectionOf courseAndPerson = df.getOWLObjectIntersectionOf(course, person);
+        OWLSubClassOfAxiom courseAndPersonSubClassOfNothing = df.getOWLSubClassOfAxiom(courseAndPerson, df.getOWLNothing());
+
+        OWLSubClassOfAxiom UGCSubClassOfCourse = df.getOWLSubClassOfAxiom(UGC, course);
+        OWLSubClassOfAxiom PGCSubClassOfCourse = df.getOWLSubClassOfAxiom(PGC, course);
+
+        OWLObjectSomeValuesFrom exisTeachesCourse = df.getOWLObjectSomeValuesFrom(teaches, course);
+        OWLObjectIntersectionOf personAndExisTeachesCourse = df.getOWLObjectIntersectionOf(person, exisTeachesCourse);
+        OWLEquivalentClassesAxiom teacherEquiv = df.getOWLEquivalentClassesAxiom(teacher, personAndExisTeachesCourse);
+
+        OWLObjectSomeValuesFrom exisTeachesThing = df.getOWLObjectSomeValuesFrom(teaches, df.getOWLThing());
+        OWLSubClassOfAxiom exisTeachesThingSubClassOfPerson = df.getOWLSubClassOfAxiom(exisTeachesThing, person);
+
+        OWLObjectSomeValuesFrom exisAttendsCourse = df.getOWLObjectSomeValuesFrom(attends, course);
+        OWLObjectIntersectionOf personAndExisAttendsCourse = df.getOWLObjectIntersectionOf(person, exisAttendsCourse);
+        OWLEquivalentClassesAxiom studentEquiv = df.getOWLEquivalentClassesAxiom(student, personAndExisAttendsCourse);
+
+        OWLObjectSomeValuesFrom exisAttendsThing = df.getOWLObjectSomeValuesFrom(attends, df.getOWLThing());
+        OWLSubClassOfAxiom exisAttendsThingSubClassOfPerson = df.getOWLSubClassOfAxiom(exisAttendsThing, person);
+
+        universityOntology.addAxioms(
+            courseAndPersonSubClassOfNothing,
+            UGCSubClassOfCourse,
+            PGCSubClassOfCourse,
+            teacherEquiv,
+            exisTeachesThingSubClassOfPerson,
+            studentEquiv,
+            exisAttendsThingSubClassOfPerson
+        );
+
+        return universityOntology;
+    }
+
+    public static OWLOntology createOWLOntologyA() {
+        OWLOntology ontology = OntologyUtils.createEmptyOWLOntology();
+        OWLOntologyManager owlOntologyManager = ontology.getOWLOntologyManager();
+        OWLDataFactory df = owlOntologyManager.getOWLDataFactory();
+        IRI iri = owlOntologyManager.getOntologyDocumentIRI(ontology);
+
+        OWLClass A = df.getOWLClass(iri + "#A");
+        OWLClass B = df.getOWLClass(iri + "#B");
+        OWLClass C = df.getOWLClass(iri + "#C");
+        OWLClass D = df.getOWLClass(iri + "#D");
+
+        OWLObjectProperty r = df.getOWLObjectProperty(iri + "#r");
+
+        OWLObjectSomeValuesFrom rA = df.getOWLObjectSomeValuesFrom(r, A);
+        OWLObjectSomeValuesFrom rB = df.getOWLObjectSomeValuesFrom(r, B);
+        OWLObjectSomeValuesFrom rC = df.getOWLObjectSomeValuesFrom(r, C);
+        OWLObjectSomeValuesFrom rrB = df.getOWLObjectSomeValuesFrom(r, rB);
+
+        OWLObjectIntersectionOf AandB = df.getOWLObjectIntersectionOf(A, B);
+        OWLObjectIntersectionOf CandD = df.getOWLObjectIntersectionOf(C, D);
+        OWLObjectIntersectionOf BandrC = df.getOWLObjectIntersectionOf(B, rC);
+        OWLObjectIntersectionOf BandrB = df.getOWLObjectIntersectionOf(B, rB);
+        OWLObjectIntersectionOf rAandB = df.getOWLObjectIntersectionOf(rA, B);
+        OWLObjectIntersectionOf rrBandD = df.getOWLObjectIntersectionOf(rrB, D);
+        OWLObjectSomeValuesFrom rexistsAandB = df.getOWLObjectSomeValuesFrom(r, AandB);
+
+        OWLSubClassOfAxiom AsubclassofBandrC = df.getOWLSubClassOfAxiom(A, BandrC);
+        OWLSubClassOfAxiom BandrBsubclassofCandD = df.getOWLSubClassOfAxiom(BandrB, CandD);
+        OWLSubClassOfAxiom CsubclassofrAandB = df.getOWLSubClassOfAxiom(C, rAandB);
+        OWLSubClassOfAxiom rrBandDsubclassofrexistsAandB = df.getOWLSubClassOfAxiom(rrBandD, rexistsAandB);
+
+        OWLNamedIndividual individual = df.getOWLNamedIndividual(iri + "#IO");
+        OWLSubClassOfAxiom IOsubclassofaxiomC = df.getOWLSubClassOfAxiom(df.getOWLObjectOneOf(individual), C);
+        ontology.add(
+            AsubclassofBandrC,
+            BandrBsubclassofCandD,
+            CsubclassofrAandB,
+            rrBandDsubclassofrexistsAandB,
+            IOsubclassofaxiomC
+        );
+
+        return ontology;
+    }
+}
