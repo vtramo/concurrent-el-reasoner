7411f72a709de49e1bad4124a43c8cbbb065e3a0
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-10 23:18:14 +0100

    feat: add ContextCR4 and ContextCR5

---
+++ src/main/java/saturation/context/ContextCR4.java
@@ -0,0 +1,156 @@
+package saturation.context;
+
+import index.GCILeftExistentialIndex;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static normalisation.NormalisationChecks.isSubclassBCConcept;
+import static normalisation.NormalisationChecks.isSuperclassBCConcept;
+
+public final class ContextCR4 implements Context {
+    private final ContextType contextType = ContextType.CR4;
+    private final OWLClassExpression contextClassExpression;
+    private final Queue<OWLSubClassOfAxiom> todoAxioms = new ConcurrentLinkedQueue<>();
+    private final GCILeftExistentialIndex gciLeftExistentialOntologyIndex;
+    private final Map<OWLObjectPropertyExpression, Set<OWLClassExpression>> subclassesByRoleProcessedAxioms = new HashMap<>();
+    private final Set<OWLSubClassOfAxiom> simpleProcessedAxioms = new HashSet<>();
+    private AtomicBoolean isActive = new AtomicBoolean(false);
+
+    public ContextCR4(OWLClassExpression classExpression) {
+        this(classExpression, new GCILeftExistentialIndex());
+    }
+
+    public ContextCR4(OWLClassExpression classExpression, GCILeftExistentialIndex gciLeftExistentialOntologyIndex) {
+        this.contextClassExpression = classExpression;
+        this.gciLeftExistentialOntologyIndex = gciLeftExistentialOntologyIndex;
+    }
+
+    @Override
+    public boolean addTodoAxiom(OWLSubClassOfAxiom axiom) {
+        checkAxiomValidity(axiom);
+
+        return todoAxioms.add(axiom);
+    }
+
+    @Override
+    public Optional<OWLSubClassOfAxiom> pollTodoAxiom() {
+        return Optional.ofNullable(todoAxioms.poll());
+    }
+
+    @Override
+    public boolean isQueueTodoAxiomsEmpty() {
+        return todoAxioms.isEmpty();
+    }
+
+    @Override
+    public boolean addProcessedAxiom(OWLSubClassOfAxiom subClassOfAxiom) {
+        checkAxiomValidity(subClassOfAxiom);
+
+        OWLClassExpression subClass = subClassOfAxiom.getSubClass();
+        OWLClassExpression superClass = subClassOfAxiom.getSuperClass();
+
+        if (superClass instanceof OWLObjectSomeValuesFrom someValuesFrom) {
+            OWLObjectPropertyExpression role = someValuesFrom.getProperty();
+            return subclassesByRoleProcessedAxioms
+                .computeIfAbsent(role, __ -> new HashSet<>())
+                .add(subClass);
+        } else {
+            return simpleProcessedAxioms.add(subClassOfAxiom);
+        }
+    }
+
+    private void checkAxiomValidity(OWLSubClassOfAxiom axiom) {
+        OWLClassExpression subClass = axiom.getSubClass();
+        OWLClassExpression superClass = axiom.getSuperClass();
+        if (!isSubclassBCConcept(subClass) || (!isSuperclassBCConcept(superClass) && !(superClass instanceof OWLObjectSomeValuesFrom))) {
+            throw new IllegalArgumentException();
+        }
+
+        if (isSubclassBCConcept(subClass) && isSuperclassBCConcept(superClass) && !Objects.equals(subClass, contextClassExpression)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (superClass instanceof OWLObjectSomeValuesFrom objectSomeValuesFrom && !Objects.equals(objectSomeValuesFrom.getFiller(), contextClassExpression)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean containsProcessedAxiom(OWLSubClassOfAxiom axiom) {
+        OWLClassExpression superClass = axiom.getSuperClass();
+
+        if (superClass instanceof OWLObjectSomeValuesFrom someValuesFrom) {
+            OWLObjectPropertyExpression role = someValuesFrom.getProperty();
+
+            if (!subclassesByRoleProcessedAxioms.containsKey(role)) {
+                return false;
+            }
+
+            Set<OWLClassExpression> processedSubclasses = subclassesByRoleProcessedAxioms.get(role);
+            return processedSubclasses.contains(superClass);
+        } else {
+            return simpleProcessedAxioms.contains(axiom);
+        }
+    }
+
+    @Override
+    public Collection<OWLSubClassOfAxiom> getProcessedAxioms() {
+        Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>(simpleProcessedAxioms);
+
+        OWLDataFactory owlDataFactory = OWLManager.getOWLDataFactory();
+        for (OWLObjectPropertyExpression role: subclassesByRoleProcessedAxioms.keySet()) {
+            for (OWLClassExpression subclass: subclassesByRoleProcessedAxioms.get(role)) {
+                OWLObjectSomeValuesFrom existsRoleContextClassExpression = owlDataFactory.getOWLObjectSomeValuesFrom(role, contextClassExpression);
+                OWLSubClassOfAxiom subClassOfAxiom = owlDataFactory.getOWLSubClassOfAxiom(subclass, existsRoleContextClassExpression);
+                processedAxioms.add(subClassOfAxiom);
+            }
+        }
+
+        return processedAxioms;
+    }
+
+    @Override
+    public void accept(ContextVisitor contextVisitor) {
+        contextVisitor.visit(this);
+    }
+
+    @Override
+    public AtomicBoolean getIsActive() {
+        return isActive;
+    }
+
+    public GCILeftExistentialIndex getGciLeftExistentialOntologyIndex() {
+        return gciLeftExistentialOntologyIndex;
+    }
+
+    public Map<OWLObjectPropertyExpression, Set<OWLClassExpression>> getSubclassesByRoleProcessedAxioms() {
+        return subclassesByRoleProcessedAxioms;
+    }
+
+    public Set<OWLSubClassOfAxiom> getSimpleProcessedAxioms() {
+        return simpleProcessedAxioms;
+    }
+
+    public ContextType getContextType() {
+        return contextType;
+    }
+
+    public OWLClassExpression getContextClassExpression() {
+        return contextClassExpression;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof ContextCR4 otherContextCR4)) return false;
+        return contextClassExpression.equals(otherContextCR4.contextClassExpression) && contextType.equals(otherContextCR4.getContextType());
+    }
+
+    @Override
+    public int hashCode() {
+        return (contextClassExpression.toString() + contextType).hashCode();
+    }
+}
---
+++ src/main/java/saturation/context/ContextCR5.java
@@ -0,0 +1,130 @@
+package saturation.context;
+
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static normalisation.NormalisationChecks.isSubclassBCConcept;
+
+public final class ContextCR5 implements Context {
+    private final ContextType contextType = ContextType.CR5;
+    private final OWLClassExpression contextClassExpression;
+    private final Queue<OWLSubClassOfAxiom> todoAxioms = new ConcurrentLinkedQueue<>();
+    private final Set<OWLSubClassOfAxiom> existentialRightProcessedAxioms = new HashSet<>();
+    private OWLSubClassOfAxiom processedSubclassOfNothingAxiom;
+    private final AtomicBoolean isActive = new AtomicBoolean(false);
+
+    public ContextCR5(OWLClassExpression contextClassExpression) {
+        this.contextClassExpression = contextClassExpression;
+    }
+
+    @Override
+    public boolean addTodoAxiom(OWLSubClassOfAxiom axiom) {
+        checkAxiomValidity(axiom);
+
+        return todoAxioms.add(axiom);
+    }
+
+    @Override
+    public Optional<OWLSubClassOfAxiom> pollTodoAxiom() {
+        return Optional.ofNullable(todoAxioms.poll());
+    }
+
+    @Override
+    public boolean isQueueTodoAxiomsEmpty() {
+        return todoAxioms.isEmpty();
+    }
+
+    @Override
+    public boolean addProcessedAxiom(OWLSubClassOfAxiom axiom) {
+        checkAxiomValidity(axiom);
+
+        OWLClassExpression superClass = axiom.getSuperClass();
+        if (superClass.isOWLNothing()) {
+            boolean added = (processedSubclassOfNothingAxiom == null);
+            processedSubclassOfNothingAxiom = axiom;
+            return added;
+        } else {
+            return existentialRightProcessedAxioms.add(axiom);
+        }
+    }
+
+    private void checkAxiomValidity(OWLSubClassOfAxiom axiom) {
+        OWLClassExpression subClass = axiom.getSubClass();
+        OWLClassExpression superClass = axiom.getSuperClass();
+        if (!isSubclassBCConcept(subClass) || (!superClass.isOWLNothing() && !(superClass instanceof OWLObjectSomeValuesFrom))) {
+            throw new IllegalArgumentException();
+        }
+
+        if (superClass instanceof OWLObjectSomeValuesFrom someValuesFrom && !Objects.equals(someValuesFrom.getFiller(), contextClassExpression)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (superClass.isOWLNothing() && !Objects.equals(subClass, contextClassExpression)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean containsProcessedAxiom(OWLSubClassOfAxiom axiom) {
+        if (axiom instanceof OWLObjectSomeValuesFrom) {
+            return existentialRightProcessedAxioms.contains(axiom);
+        } else {
+            return Objects.equals(axiom, processedSubclassOfNothingAxiom);
+        }
+    }
+
+    @Override
+    public Collection<OWLSubClassOfAxiom> getProcessedAxioms() {
+        Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>(existentialRightProcessedAxioms);
+
+        if (processedSubclassOfNothingAxiom != null) {
+            processedAxioms.add(processedSubclassOfNothingAxiom);
+        }
+
+        return processedAxioms;
+    }
+
+    public Set<OWLSubClassOfAxiom> getExistentialRightProcessedAxioms() {
+        Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>(existentialRightProcessedAxioms);
+
+        if (processedSubclassOfNothingAxiom != null) {
+            processedAxioms.add(processedSubclassOfNothingAxiom);
+        }
+
+        return processedAxioms;
+    }
+
+    @Override
+    public void accept(ContextVisitor contextVisitor) {
+        contextVisitor.visit(this);
+    }
+
+    @Override
+    public AtomicBoolean getIsActive() {
+        return isActive;
+    }
+
+    public Optional<OWLSubClassOfAxiom> getProcessedSubclassOfNothingAxiom() {
+        return Optional.ofNullable(processedSubclassOfNothingAxiom);
+    }
+
+    public ContextType getContextType() {
+        return contextType;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof ContextCR5 otherContextCR5)) return false;
+        return contextClassExpression.equals(otherContextCR5.contextClassExpression) && contextType.equals(otherContextCR5.getContextType());
+    }
+
+    @Override
+    public int hashCode() {
+        return (contextClassExpression.toString() + contextType).hashCode();
+    }
+}
