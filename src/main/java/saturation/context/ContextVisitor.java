5c1f07cc5ee8b1134c631bb1197751491ce9c261
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-10 23:05:02 +0100

    feat: add ContextVisitor interface
    this interface exposes methods for visiting different implementations of the Context interface: ContextCR1, ContextCR2, ContextCR3, ContextCR4, ContextCR5, and ContextCR6.

---
+++ src/main/java/saturation/context/ContextVisitor.java
@@ -0,0 +1,12 @@
+package saturation.context;
+
+public interface ContextVisitor {
+
+    void visit(ContextCR1 contextCR1);
+    void visit(ContextCR2 contextCR2);
+    void visit(ContextCR3 contextCR3);
+    void visit(ContextCR4 contextCR4);
+    void visit(ContextCR5 contextCR5);
+    void visit(ContextCR6 contextCR6);
+
+}
