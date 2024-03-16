f7e50dd05aecbc0a5fdcebb96515facaa41a2bd4
Vincenzo Tramo <vv.tramo@gmail.com>
2024-03-16 17:53:57 +0100

    test: add a test suite for the saturation process

---
+++ src/test/java/OntologySaturationTest.java
@@ -0,0 +1,87 @@
+import hierarchy.SubsumptionHierarchy;
+import hierarchy.SubsumptionHierarchyProcess;
+import index.OntologyIndex;
+import index.OntologyIndexer;
+import normalisation.OntologyNormaliser;
+import org.junit.jupiter.api.Test;
+import org.semanticweb.HermiT.ReasonerFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.parameters.Imports;
+import org.semanticweb.owlapi.reasoner.InferenceDepth;
+import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import saturation.OntologySaturationProcess;
+import saturation.SaturationResult;
+import utils.OntologyUtils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class OntologySaturationTest {
+
+    @Test
+    public void testWithoutCR6Inferences() {
+        OWLOntology ontologyWithoutCR6Inferences = OntologyUtils.createOWLOntologyAWithoutCR6Inferences();
+        OntologyNormaliser ontologyNormaliser = new OntologyNormaliser(ontologyWithoutCR6Inferences);
+        OWLOntology normalisedOntologyA = ontologyNormaliser.createNormalisedOntology();
+        OntologyIndexer ontologyAIndexer = new OntologyIndexer(normalisedOntologyA);
+        OntologyIndex ontologyAIndex = ontologyAIndexer.buildIndex();
+        OntologySaturationProcess ontologyASaturationProcess = new OntologySaturationProcess(normalisedOntologyA, ontologyAIndex);
+        SaturationResult saturation = ontologyASaturationProcess.saturate();
+        SubsumptionHierarchyProcess subsumptionHierarchyProcess = new SubsumptionHierarchyProcess();
+        SubsumptionHierarchy subsumptionHierarchy = subsumptionHierarchyProcess.buildHierarchy(saturation);
+
+        ReasonerFactory reasonerFactory = new ReasonerFactory();
+        OWLReasoner reasoner = reasonerFactory.createReasoner(normalisedOntologyA);
+        reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getSubClasses(owlClass), is(equalTo(reasoner.getSubClasses(owlClass))));
+            assertThat(subsumptionHierarchy.getSuperClasses(owlClass), is(equalTo(reasoner.getSuperClasses(owlClass))));
+            assertThat(subsumptionHierarchy.getDirectSuperClasses(owlClass), is(equalTo(reasoner.getSuperClasses(owlClass, InferenceDepth.DIRECT))));
+            assertThat(subsumptionHierarchy.getDirectSubClasses(owlClass), is(equalTo(reasoner.getSubClasses(owlClass, InferenceDepth.DIRECT))));
+        });
+    }
+
+    @Test
+    public void testWithoutCR6InferencesModified() {
+        OWLOntology ontologyWithoutCR6Inferences = OntologyUtils.createOWLOntologyAWithoutCR6InferencesModified();
+        OntologyNormaliser ontologyNormaliser = new OntologyNormaliser(ontologyWithoutCR6Inferences);
+        OWLOntology normalisedOntologyA = ontologyNormaliser.createNormalisedOntology();
+        OntologyIndexer ontologyAIndexer = new OntologyIndexer(normalisedOntologyA);
+        OntologyIndex ontologyAIndex = ontologyAIndexer.buildIndex();
+        OntologySaturationProcess ontologyASaturationProcess = new OntologySaturationProcess(normalisedOntologyA, ontologyAIndex);
+        SaturationResult saturation = ontologyASaturationProcess.saturate();
+        SubsumptionHierarchyProcess subsumptionHierarchyProcess = new SubsumptionHierarchyProcess();
+        SubsumptionHierarchy subsumptionHierarchy = subsumptionHierarchyProcess.buildHierarchy(saturation);
+
+        ReasonerFactory reasonerFactory = new ReasonerFactory();
+        OWLReasoner reasoner = reasonerFactory.createReasoner(normalisedOntologyA);
+        reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+
+        saturation.getAllAxioms().forEach(axiom -> {
+            assertThat(reasoner.isEntailed(axiom), is(equalTo(true)));
+        });
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getEquivalentClasses(owlClass), is(equalTo(reasoner.getEquivalentClasses(owlClass))));
+        });
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getDirectSubClasses(owlClass), is(equalTo(reasoner.getSubClasses(owlClass, InferenceDepth.DIRECT))));
+        });
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getDirectSuperClasses(owlClass), is(equalTo(reasoner.getSuperClasses(owlClass, InferenceDepth.DIRECT))));
+        });
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getSuperClasses(owlClass), is(equalTo(reasoner.getSuperClasses(owlClass))));
+        });
+
+        normalisedOntologyA.getClassesInSignature(Imports.INCLUDED).forEach(owlClass -> {
+            assertThat(subsumptionHierarchy.getSubClasses(owlClass), is(equalTo(reasoner.getSubClasses(owlClass))));
+        });
+    }
+}
--- src/test/java/utils/OntologyUtils.java
+++ src/test/java/utils/OntologyUtils.java
@@ -64,7 +64,104 @@ public class OntologyUtils {
         return universityOntology;
     }
 
-    public static OWLOntology createOWLOntologyA() {
+    public static OWLOntology createOWLOntologyAWithoutCR6Inferences() {
+        OWLOntology ontology = OntologyUtils.createEmptyOWLOntology();
+        OWLOntologyManager owlOntologyManager = ontology.getOWLOntologyManager();
+        OWLDataFactory df = owlOntologyManager.getOWLDataFactory();
+        IRI iri = owlOntologyManager.getOntologyDocumentIRI(ontology);
+
+        OWLClass A = df.getOWLClass(iri + "#A");
+        OWLClass B = df.getOWLClass(iri + "#B");
+        OWLClass C = df.getOWLClass(iri + "#C");
+        OWLClass D = df.getOWLClass(iri + "#D");
+
+        OWLObjectProperty r = df.getOWLObjectProperty(iri + "#r");
+
+        OWLObjectSomeValuesFrom rA = df.getOWLObjectSomeValuesFrom(r, A);
+        OWLObjectSomeValuesFrom rB = df.getOWLObjectSomeValuesFrom(r, B);
+        OWLObjectSomeValuesFrom rC = df.getOWLObjectSomeValuesFrom(r, C);
+        OWLObjectSomeValuesFrom rrB = df.getOWLObjectSomeValuesFrom(r, rB);
+
+        OWLObjectIntersectionOf AandB = df.getOWLObjectIntersectionOf(A, B);
+        OWLObjectIntersectionOf AandC = df.getOWLObjectIntersectionOf(A, C);
+        OWLObjectIntersectionOf CandD = df.getOWLObjectIntersectionOf(C, D);
+        OWLObjectIntersectionOf BandrC = df.getOWLObjectIntersectionOf(B, rC);
+        OWLObjectIntersectionOf BandrB = df.getOWLObjectIntersectionOf(B, rB);
+        OWLObjectIntersectionOf rAandB = df.getOWLObjectIntersectionOf(rA, B);
+        OWLObjectIntersectionOf rrBandD = df.getOWLObjectIntersectionOf(rrB, D);
+        OWLObjectSomeValuesFrom rexistsAandB = df.getOWLObjectSomeValuesFrom(r, AandB);
+
+        OWLSubClassOfAxiom AsubclassofBandrC = df.getOWLSubClassOfAxiom(A, BandrC);
+        OWLSubClassOfAxiom BandrBsubclassofCandD = df.getOWLSubClassOfAxiom(BandrB, CandD);
+        OWLSubClassOfAxiom CsubclassofrAandB = df.getOWLSubClassOfAxiom(C, rAandB);
+        OWLSubClassOfAxiom rrBandDsubclassofrexistsAandB = df.getOWLSubClassOfAxiom(rrBandD, rexistsAandB);
+
+        OWLNamedIndividual individual = df.getOWLNamedIndividual(iri + "#IO");
+        OWLObjectOneOf io = df.getOWLObjectOneOf(individual);
+        OWLSubClassOfAxiom IOsubclassofaxiomC = df.getOWLSubClassOfAxiom(io, C);
+
+        ontology.add(
+            AsubclassofBandrC,
+            BandrBsubclassofCandD,
+            CsubclassofrAandB,
+            rrBandDsubclassofrexistsAandB,
+            IOsubclassofaxiomC
+        );
+
+        return ontology;
+    }
+
+    public static OWLOntology createOWLOntologyAWithoutCR6InferencesModified() {
+        OWLOntology ontology = OntologyUtils.createEmptyOWLOntology();
+        OWLOntologyManager owlOntologyManager = ontology.getOWLOntologyManager();
+        OWLDataFactory df = owlOntologyManager.getOWLDataFactory();
+        IRI iri = owlOntologyManager.getOntologyDocumentIRI(ontology);
+
+        OWLClass A = df.getOWLClass(iri + "#A");
+        OWLClass B = df.getOWLClass(iri + "#B");
+        OWLClass C = df.getOWLClass(iri + "#C");
+        OWLClass D = df.getOWLClass(iri + "#D");
+
+        OWLObjectProperty r = df.getOWLObjectProperty(iri + "#r");
+
+        OWLObjectSomeValuesFrom rA = df.getOWLObjectSomeValuesFrom(r, A);
+        OWLObjectSomeValuesFrom rB = df.getOWLObjectSomeValuesFrom(r, B);
+        OWLObjectSomeValuesFrom rC = df.getOWLObjectSomeValuesFrom(r, C);
+        OWLObjectSomeValuesFrom rrB = df.getOWLObjectSomeValuesFrom(r, rB);
+
+        OWLObjectIntersectionOf AandB = df.getOWLObjectIntersectionOf(A, B);
+        OWLObjectIntersectionOf AandC = df.getOWLObjectIntersectionOf(A, C);
+        OWLObjectIntersectionOf CandD = df.getOWLObjectIntersectionOf(C, D);
+        OWLObjectIntersectionOf BandrC = df.getOWLObjectIntersectionOf(B, rC);
+        OWLObjectIntersectionOf BandrB = df.getOWLObjectIntersectionOf(B, rB);
+        OWLObjectIntersectionOf rAandB = df.getOWLObjectIntersectionOf(rA, B);
+        OWLObjectIntersectionOf rrBandD = df.getOWLObjectIntersectionOf(rrB, D);
+        OWLObjectSomeValuesFrom rexistsAandB = df.getOWLObjectSomeValuesFrom(r, AandB);
+        OWLObjectSomeValuesFrom rexistsAandC = df.getOWLObjectSomeValuesFrom(r, AandC);
+
+        OWLSubClassOfAxiom AsubclassofBandrC = df.getOWLSubClassOfAxiom(A, BandrC);
+        OWLSubClassOfAxiom BandrBsubclassofCandD = df.getOWLSubClassOfAxiom(BandrB, CandD);
+        OWLSubClassOfAxiom CsubclassofrAandB = df.getOWLSubClassOfAxiom(C, rAandB);
+        OWLSubClassOfAxiom DsubclassofrexistsAandC = df.getOWLSubClassOfAxiom(D, rexistsAandC);
+        OWLSubClassOfAxiom rrBandDsubclassofrexistsAandB = df.getOWLSubClassOfAxiom(rrBandD, rexistsAandB);
+
+        OWLNamedIndividual individual = df.getOWLNamedIndividual(iri + "#IO");
+        OWLObjectOneOf io = df.getOWLObjectOneOf(individual);
+        OWLSubClassOfAxiom IOsubclassofaxiomC = df.getOWLSubClassOfAxiom(io, C);
+
+        ontology.add(
+            AsubclassofBandrC,
+            BandrBsubclassofCandD,
+            CsubclassofrAandB,
+            rrBandDsubclassofrexistsAandB,
+            IOsubclassofaxiomC,
+            DsubclassofrexistsAandC
+        );
+
+        return ontology;
+    }
+
+    public static OWLOntology createOWLOntologyAWithAllInferences() {
         OWLOntology ontology = OntologyUtils.createEmptyOWLOntology();
         OWLOntologyManager owlOntologyManager = ontology.getOWLOntologyManager();
         OWLDataFactory df = owlOntologyManager.getOWLDataFactory();
@@ -96,13 +193,19 @@ public class OntologyUtils {
         OWLSubClassOfAxiom rrBandDsubclassofrexistsAandB = df.getOWLSubClassOfAxiom(rrBandD, rexistsAandB);
 
         OWLNamedIndividual individual = df.getOWLNamedIndividual(iri + "#IO");
-        OWLSubClassOfAxiom IOsubclassofaxiomC = df.getOWLSubClassOfAxiom(df.getOWLObjectOneOf(individual), C);
+        OWLObjectOneOf io = df.getOWLObjectOneOf(individual);
+        OWLSubClassOfAxiom IOsubclassofaxiomC = df.getOWLSubClassOfAxiom(io, C);
+        OWLSubClassOfAxiom CsubclassofIO = df.getOWLSubClassOfAxiom(C, io);
+        OWLSubClassOfAxiom AsubclassofIO = df.getOWLSubClassOfAxiom(A, io);
+
         ontology.add(
             AsubclassofBandrC,
             BandrBsubclassofCandD,
             CsubclassofrAandB,
             rrBandDsubclassofrexistsAandB,
-            IOsubclassofaxiomC
+            IOsubclassofaxiomC,
+            CsubclassofIO,
+            AsubclassofIO
         );
 
         return ontology;
